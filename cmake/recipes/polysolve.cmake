d741a031f4b5d55d557e7801e06c1af7e35babaa
fsichetti <f.sichetti@gmail.com>
2024-08-22 17:53:08 +0200

    cmake updates

--- cmake/recipes/polysolve.cmake
+++ cmake/recipes/polysolve.cmake
@@ -11,4 +11,4 @@ message(STATUS "Third-party: creating target 'polysolve'")
 set(POLYSOLVE_WITH_ACCELERATE OFF CACHE BOOL "Enable Apple Accelerate" FORCE)
 
 include(CPM)
-CPMAddPackage("gh:polyfem/polysolve#9f8a5e18d064a38f12a9abf5bcf1ff87a7432219")
+CPMAddPackage("gh:polyfem/polysolve#0cd21baa0f90a322972e2b858ba449260b95b6f8")
